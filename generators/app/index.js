2d2454fe586d9a05896c35bc89f97bb8d1704505
Mateus Freira <mateus.freira@gmail.com>
2015-12-10 21:41:41 -0200

    docbase_logo

--- generators/app/index.js
+++ generators/app/index.js
@@ -159,12 +159,8 @@ module.exports = yeoman.generators.Base.extend({
 
     projectfiles: function() {
       this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
+        this.templatePath('images/_docbase.png'),
+        this.destinationPath('images/docbase.png')
       );
     }
   },
